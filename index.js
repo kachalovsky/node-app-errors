f442ea6af15a490570b7ebb1bdd6064c586c9c04
Andrey Kachalovsky <andrei7a@outlook.com>
2017-09-02 23:44:02 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
 'use strict';
 class ApplicationError {
-    constructor(context, history) {
-        this.status = context.status;
-        this.message = context.message;
-        this._history = history || [];
-        this.IS_APPLICATION_ERROR = true;
-    }
-    get context() {
-        return {status: this.status, message: this.message};
-    }
+  constructor(context, history) {
+    this.status = context.status;
+    this.message = context.message;
+    this._history = history || [];
+    this.IS_APPLICATION_ERROR = true;
+  }
+  get context() {
+    return {status: this.status, message: this.message};
+  }
 
-    get history() {
-        return this._history;
-    }
+  get history() {
+    return this._history;
+  }
 
-    static screen(err, otherwise) {
-        if (err && err.IS_APPLICATION_ERROR) return err;
-        otherwise = (otherwise.IS_APPLICATION_ERROR) ? otherwise : new this(otherwise);
-        if (err) {
-            let history = otherwise.history.slice() || [];
-            history.push(err);
-            return new this(otherwise.context || otherwise, history);
-        }
-        return otherwise
+  static screen(err, otherwise) {
+    if (err && err.IS_APPLICATION_ERROR) return err;
+    otherwise = (otherwise.IS_APPLICATION_ERROR) ? otherwise : new this(otherwise);
+    if (err) {
+      let history = otherwise.history.slice() || [];
+      history.push(err);
+      return new this(otherwise.context || otherwise, history);
     }
+    return otherwise
+  }
 
-    static reject(err, otherwise) {
-        return Promise.reject(this.screen(err, otherwise));
-    }
+  static reject(err, otherwise) {
+    return Promise.reject(this.screen(err, otherwise));
+  }
 
-    static generateResourceErrors(modelName) {
-        return ({
-            CREATE: new this({status: 500, message: `Error with ${modelName} creating.`}),
-            FETCH: new this({status: 500, message: `Error around ${modelName} fetching.`}),
-            UPDATE: new this({status: 500, message: `Error with ${modelName} updating.`}),
-            DELETE: new this({status: 500, message: `Error with ${modelName} deleting.`}),
-            NOT_FOUND: new this({status: 404, message: `${modelName} not found.`})
-        })
-    }
+  static isEquals(err, other) {
+    if (err.status === other.status && err.message === other.message) return Promise.resolve();
+    return Promise.reject(err);
+  }
 
-    static createBulkErrors(errors) {
-        return Object.keys(errors).reduce((result, key) => {
-            result[key] = new this(errors[key]);
-            return result;
-        }, {})
-    }
+  static generateResourceErrors(modelName) {
+    return ({
+      CREATE: new this({status: 500, message: `Error with ${modelName} creating.`}),
+      FETCH: new this({status: 500, message: `Error around ${modelName} fetching.`}),
+      UPDATE: new this({status: 500, message: `Error with ${modelName} updating.`}),
+      DELETE: new this({status: 500, message: `Error with ${modelName} deleting.`}),
+      NOT_FOUND: new this({status: 404, message: `${modelName} not found.`})
+    })
+  }
 
-    static remapError(error, remappedErrors, otherwise) {
-        const remapped = remappedErrors || {};
-        let remappedError = (remapped[error.message] || {})[((error.errors || [])[0] || {}).message];
-        if (remappedError) {
-            if (!remappedError.IS_APPLICATION_ERROR) remappedError = new this(remappedError)
-        }
-        return this.reject(remappedError, otherwise)
+  static createBulkErrors(errors) {
+    return Object.keys(errors).reduce((result, key) => {
+      result[key] = new this(errors[key]);
+      return result;
+    }, {})
+  }
+
+  static remapError(error, remappedErrors, otherwise) {
+    const remapped = remappedErrors || {};
+    let remappedError = (remapped[error.message] || {})[((error.errors || [])[0] || {}).message];
+    if (remappedError) {
+      if (!remappedError.IS_APPLICATION_ERROR) remappedError = new this(remappedError)
     }
+    return this.reject(remappedError, otherwise)
+  }
 }
 
 
